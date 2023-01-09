3ef8540b61fc585d39119b66d3bcdefe761237e9
Aldrin Caballero <aldrincaballero94@gmail.com>
2023-01-09 11:36:07 +0800

    client folder added

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'averia-gruesa-libre': ['Averia Gruesa Libre', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
